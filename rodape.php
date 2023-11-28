72f56268f1307a89fb36bd4abc3cae9cb2d86f66
douglasbks <douglasbks2016@gmail.com>
2023-11-28 18:18:21 -0300

    rodape

--- rodape.php
+++ rodape.php
@@ -2,15 +2,15 @@
     <p class="tags">Mangangá | Patrono | Projetos Educacionais | Música nas Empresas | Portfólio Meninos</p>
     <div class="row">
         <div class="col-4">
-            <p><b>E-mail: </b>adm@capoeiramanganga.com.br</p>
-            <p><b>Sede: </b>Rua Professor Soeiro nº57 - Pau Miúdo - Salvador/BA</p>
-            <p><b>Telefone: </b>+55 71 99999-9999</p>
+            <p class="texto_rodape"><b>E-mail: </b><span class="dados_rodape">adm@capoeiramanganga.com.br</span></p>
+            <p class="texto_rodape"><b>Sede: </b><span class="dados_rodape">Rua Professor Soeiro nº57 - Pau Miúdo - Salvador/BA</span></p>
+            <p class="texto_rodape"><b>Telefone: </b><span class="dados_rodape">+55 71 99999-9999</span></p>
         </div>
-        <div class="col-4">
-            <img src="img/logo.png">
+        <div class="col-4 d-flex justify-content-center align-items-center">
+            <img src="img/logo_preto_branco.png">
         </div>
-        <div class="col-4">
-            <p>COPYRIGHT <?= date('Y') ?> MANGANGÁ TODOS OS DIREITOS RESERVADOS.</p>
+        <div class="col-4 d-flex justify-content-center align-items-center">
+            <p class="copyright">COPYRIGHT &copy; <?= date('Y') ?> MANGANGÁ TODOS OS DIREITOS RESERVADOS.</p>
         </div>
     </div>
 </div>
