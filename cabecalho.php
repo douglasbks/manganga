cb818c7f5fc7619aa08f692c97797c9af05a2a00
douglasbks <douglasbks2016@gmail.com>
2023-11-28 14:16:46 -0300

    cabecalho

--- cabecalho.php
+++ cabecalho.php
@@ -4,7 +4,26 @@
             <img src="img/logo.png">
         </div>
         <div class="col-6 col-md-10">
-            <h1>ASSOCIAÇÃO SÓCIO CULTURAL DE CAPOEIRA BLOCO CARNAVALESCO AFRO <span class="maganga-roxo">MANGANGÁ</span></h1>
+            <h1>ASSOCIAÇÃO SÓCIO CULTURAL DE CAPOEIRA BLOCO CARNAVALESCO AFRO <span class="maganga-roxo">MANGANGÁ</span>
+            </h1>
+            <div class="d-none d-sm-block">
+                <img src="img/home.png" class="botao_icone">
+                <button class="botao_menu botao_azul">MANGANGÁ</button>
+                <button class="botao_menu botao_verde">PATRONO TONHO MATÉRIA</button>
+                <button class="botao_menu botao_laranja">PROJETOS EDUCACIONAIS</button>
+                <button class="botao_menu botao_amarelo">MÚSICA NAS EMPRESAS</button>
+                <button class="botao_menu botao_rosa">PORTFÓLIO MENINOS</button>
+                <img src="img/contato.png" class="botao_icone">
+            </div>
+        </div>
+        <div class="d-block d-sm-none">
+            <img src="img/home.png" class="botao_icone_celular">
+            <button class="botao_menu_celular botao_azul">MANGANGÁ</button>
+            <button class="botao_menu_celular botao_verde">PATRONO TONHO MATÉRIA</button>
+            <button class="botao_menu_celular botao_laranja">PROJETOS EDUCACIONAIS</button>
+            <button class="botao_menu_celular botao_amarelo">MÚSICA NAS EMPRESAS</button>
+            <button class="botao_menu_celular botao_rosa">PORTFÓLIO MENINOS</button>
+            <img src="img/contato.png" class="botao_icone_celular">
         </div>
     </div>
 </div>
