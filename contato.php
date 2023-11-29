2de211f382090b43998ff09fa5dc2935a5fe409e
douglasbks <douglasbks2016@gmail.com>
2023-11-29 01:23:37 -0300

    ajustes visuais

--- contato.php
+++ contato.php
@@ -16,21 +16,25 @@
                 <div class="textarea-wrapper">
                     <label for="mensagem" class="form-label">Mensagem</label>
                     <div class="textarea-container">
-                        <textarea class="form-control" id="mensagem" rows="7"></textarea>
-                        <button type="submit" class="btn-enviar">ENVIAR<img src="img/enviar.png"></button>
+                        <div class="textarea-flex">
+                            <textarea class="form-control" id="mensagem" rows="7"></textarea>
+                            <button type="submit" class="btn-enviar">ENVIAR<img src="img/enviar.png"></button>
+                        </div>
                     </div>
                 </div>
             </div>
             <div class="col-4">
-                <span class="form-label">Endereços</span>
                 <div class="endereco">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.1937147029207!2d-38.4838999!3d-12.959452899999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7161ad2bdcbbd09%3A0xe1c7afee494d5076!2sR.%20Prof.%20Soeiro%2C%2057%20-%20Pau%20Mi%C3%BAdo%2C%20Salvador%20-%20BA%2C%2040310-300!5e0!3m2!1spt-BR!2sbr!4v1701194565299!5m2!1spt-BR!2sbr"
-                            width="94%" height="300" style="margin: 3%" allowfullscreen="" loading="lazy"
-                            referrerpolicy="no-referrer-when-downgrade"></iframe>
-                    <div class="legenda">
-                        <p><img src="img/icon_local_red.png"> Endereço Sede Salvador</p>
-                        <p><img src="img/icon_local_black.png"> Endereço Filial Simões Filho</p>
-                        <p><img src="img/icon_local_black.png"> Endereço Filial Lauro de Freitas</p>
+                    <span class="form-label endereco-label">Endereços</span>
+                    <div class="form-control endereco-content">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.1937147029207!2d-38.4838999!3d-12.959452899999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7161ad2bdcbbd09%3A0xe1c7afee494d5076!2sR.%20Prof.%20Soeiro%2C%2057%20-%20Pau%20Mi%C3%BAdo%2C%20Salvador%20-%20BA%2C%2040310-300!5e0!3m2!1spt-BR!2sbr!4v1701194565299!5m2!1spt-BR!2sbr"
+                                width="94%" height="250" style="margin: 3%" allowfullscreen="" loading="lazy"
+                                referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <div class="legenda">
+                            <p><img src="img/icon_local_red.png"> Endereço Sede Salvador</p>
+                            <p><img src="img/icon_local_black.png"> Endereço Filial Simões Filho</p>
+                            <p><img src="img/icon_local_black.png"> Endereço Filial Lauro de Freitas</p>
+                        </div>
                     </div>
                 </div>
             </div>
