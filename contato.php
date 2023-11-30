1cfa0c8167578ea4c81a97e0c172e05a20d73707
douglasbks <douglasbks2016@gmail.com>
2023-11-30 01:57:30 -0300

    ajuste box endereco

--- contato.php
+++ contato.php
@@ -29,7 +29,7 @@
                     <span class="form-label endereco-label">Endereços</span>
                     <div class="form-control endereco-content">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.1937147029207!2d-38.4838999!3d-12.959452899999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7161ad2bdcbbd09%3A0xe1c7afee494d5076!2sR.%20Prof.%20Soeiro%2C%2057%20-%20Pau%20Mi%C3%BAdo%2C%20Salvador%20-%20BA%2C%2040310-300!5e0!3m2!1spt-BR!2sbr!4v1701194565299!5m2!1spt-BR!2sbr"
-                                width="95%" height="250" style="margin: 0.5rem" allowfullscreen="" loading="lazy"
+                                width="380" height="250" allowfullscreen="" loading="lazy"
                                 referrerpolicy="no-referrer-when-downgrade"></iframe>
                         <div class="legenda">
                             <p><img src="img/icon_local_red.png"> Endereço Sede Salvador</p>
