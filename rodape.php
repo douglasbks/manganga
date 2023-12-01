26408501a8650197b98ce772237853b0884e300c
douglasbks <douglasbks2016@gmail.com>
2023-12-01 00:57:08 -0300

    ajuste

--- rodape.php
+++ rodape.php
@@ -1,7 +1,7 @@
 <div class="rodape">
     <p class="tags"><span>Mangangá</span> | <span>Patrono</span> | <span>Projetos Educacionais</span> | <span>Música nas Empresas</span> | <span>Portfólio Meninos</span></p>
     <div class="row">
-        <div class="col-5">
+        <div class="col-5 info-rodape">
             <p class="texto_rodape"><b>E-mail: </b><span class="dados_rodape">adm@capoeiramanganga.com.br</span></p>
             <p class="texto_rodape"><b>Sede: </b><span class="dados_rodape">Rua Professor Soeiro nº57 - Pau Miúdo - Salvador/BA</span></p>
             <p class="texto_rodape"><b>Telefone: </b><span class="dados_rodape">+55 71 99999-9999</span></p>
